06c255f1d089b83e02ba11a9ff252bced0f6b81b
alexarlord-boop <rezerw0.1@yandex.ru>
2024-04-23 18:09:58 +0300

    GEANT-2024 Updated dkr.sh -- relative path for proxyconfigs folder

--- proxy-container/dkr.sh
+++ proxy-container/dkr.sh
@@ -7,12 +7,13 @@ docker rmi -f proxy
 docker build -t proxy .
 
 docker run -d -p 8000:8000 -p 80:80 -p 443:443 \
-         -v /proxyconfigs/letsencrypt/:/etc/letsencrypt/ \
-         -v /proxyconfigs/simplesamlphp/config/:/var/simplesamlphp/config/ \
-         -v /proxyconfigs/simplesamlphp/metadata/:/var/simplesamlphp/metadata/ \
-         -v /proxyconfigs/simplesamlphp/cert/:/var/simplesamlphp/cert/ \
-         -v /proxyconfigs/simplesamlphp/modules/mymodule:/var/simplesamlphp/modules/mymodule \
-         -v /proxyconfigs/simplesamlphp/config/:/var/www/laravel-app/public/config \
-         -v /proxyconfigs/simplesamlphp/metadata/shared:/var/www/laravel-app/public/metadata/shared \
+         -v ./proxyconfigs/letsencrypt/:/etc/letsencrypt/ \
+         -v ./proxyconfigs/simplesamlphp/config/:/var/simplesamlphp/config/ \
+         -v ./proxyconfigs/simplesamlphp/metadata/:/var/simplesamlphp/metadata/ \
+         -v ./proxyconfigs/simplesamlphp/cert/:/var/simplesamlphp/cert/ \
+         -v ./proxyconfigs/simplesamlphp/modules/mymodule:/var/simplesamlphp/modules/mymodule \
+         -v ./proxyconfigs/simplesamlphp/config/:/var/www/laravel-app/public/config \
+         -v ./proxyconfigs/simplesamlphp/metadata/shared:/var/www/laravel-app/public/metadata/shared \
+         -v ./proxyconfigs/logs/laravel/:/var/www/laravel-app/storage/logs/ \
          --name proxy proxy
 
